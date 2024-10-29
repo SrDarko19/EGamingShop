c41e4d83474ef9dd3a41888017b0708356bdf78c
SrDarko19 <luis.salinas23@itca.edu.sv>
2024-10-29 14:18:06 -0600

    first commit

--- compra_detalle.php
+++ compra_detalle.php
@@ -34,7 +34,7 @@ $sqlDetalle->execute([$idCompra]);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E gaming Shop</title>
+    <title>Fusion Games</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilos.css">
 </head>
--- recupera.php
+++ recupera.php
@@ -39,7 +39,7 @@ if (!empty($_POST)) {
 
                     $url = SITE_URL . '/reset_password.php?id=' . $user_id . '&token=' . $token;
 
-                    $asunto = "Recuperar password - E gaming Shop";
+                    $asunto = "Recuperar password - Fusion Games";
                     $cuerpo = "Estimado $nombres:<br> Si has solicitado el cambio de tu contraseña da clic en el siguiente link <a href='$url'>$url</a>.";
                     $cuerpo .= "<br>Si no hiciste esta solicitud puedes ignorar este correo.";
 
@@ -62,7 +62,7 @@ if (!empty($_POST)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E gaming Shop</title>
+    <title>Fusion Games</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilos.css">
     <style>
